dec32b36e8376031d71e14ce368063e2ceb77fbc
Jakub Solecki <jakub.solecki@onet.com.pl>
2020-11-12 10:25:57 +0100

    gui-gallery finished

--- laby/lab-gui-gallery/src/main/java/controller/GalleryController.java
+++ laby/lab-gui-gallery/src/main/java/controller/GalleryController.java
@@ -1,9 +1,13 @@
 package controller;
 
 
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import io.reactivex.rxjavafx.schedulers.JavaFxScheduler;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
@@ -11,6 +15,7 @@ import model.Gallery;
 import model.Photo;
 
 import javafx.scene.control.TextField;
+import util.PhotoDownloader;
 
 public class GalleryController {
 
@@ -24,6 +29,18 @@ public class GalleryController {
 
     @FXML
     private ListView<Photo> imagesListView;
+
+    @FXML
+    private TextField searchTextField;
+
+    public void searchButtonClicked(ActionEvent event) {
+        PhotoDownloader photoDownloader = new PhotoDownloader();
+        galleryModel.clear();
+        photoDownloader.searchForPhotos(searchTextField.getText())
+                .subscribeOn(Schedulers.io())
+                .observeOn(JavaFxScheduler.platform())
+                .subscribe(galleryModel::addPhoto);
+    }
 
     @FXML
     public void initialize() {
@@ -46,7 +63,9 @@ public class GalleryController {
         imagesListView.getSelectionModel()
                 .selectedItemProperty()
                 .addListener((observable, oldValue, newValue) -> {
-                    bindSelectedPhoto(newValue);
+                    if (newValue != null) {
+                        bindSelectedPhoto(newValue);
+                    }
                     if (oldValue != null) {
                         imageNameField.textProperty().unbindBidirectional(oldValue);
                     }
@@ -61,8 +80,8 @@ public class GalleryController {
 
     private void bindSelectedPhoto(Photo selectedPhoto) {
         imagesListView.getSelectionModel().select(selectedPhoto);
-        imageView.imageProperty().bindBidirectional(selectedPhoto.photoDataProperty());
-        imageNameField.textProperty().bind(selectedPhoto.nameProperty());
+        imageView.imageProperty().bind(selectedPhoto.photoDataProperty());
+        imageNameField.textProperty().bindBidirectional(selectedPhoto.nameProperty());
     }
 
 
